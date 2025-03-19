7fca87ea2f37f7fed007d54e42c60bcebd0e1777
Nicolae Rusu <nicolae@rusu.dev>
2025-03-19 18:32:50 -0400

    init frontend

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    sugar.url = "github:itsneeku/flake-sugar";
+  };
+
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    inputs.utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        devDeps = with pkgs; [
+          pnpm
+          nodejs_latest
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = devDeps;
+          shellHook = inputs.sugar.welcome devDeps;
+        };
+      }
+    );
+}
