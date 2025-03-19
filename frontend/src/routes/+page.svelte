987179090ff416f49824d74fccfaf1a46916860a
Nicolae Rusu <nicolae@rusu.dev>
2025-03-19 19:48:54 -0400

    no a11y allowed

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -16,7 +16,6 @@
 		<Button onclick={toggleMode} variant="outline" size="icon">
 			<Sun class=" rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 			<Moon class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-			<span class="sr-only">Toggle theme</span>
 		</Button>
 	</div>
 	<div class=" grow">
