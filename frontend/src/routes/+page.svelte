b5b4ce3caa7f66edf5cacddca6c7e82f8e4e6190
Nicolae Rusu <nicolae@rusu.dev>
2025-03-19 19:30:04 -0400

    feat: base page structure

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,58 @@
-<script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+<script>
+	import { Button } from '$lib/components/ui/button/';
+	import Sun from '@lucide/svelte/icons/sun';
+	import Moon from '@lucide/svelte/icons/moon';
+	import Send from '@lucide/svelte/icons/send';
+	import { toggleMode } from 'mode-watcher';
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label';
+	import { Textarea } from '$lib/components/ui/textarea';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button variant="secondary">Click me</Button>
+<div
+	class="flex h-screen w-full flex-col items-center justify-center bg-zinc-100 p-4 dark:bg-zinc-900"
+>
+	<div class="h-16 self-end">
+		<Button onclick={toggleMode} variant="outline" size="icon">
+			<Sun class=" rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<Moon class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</div>
+	<div class="flex-grow">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Create project</Card.Title>
+				<Card.Description>Deploy your new project in one-click.</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<div class="grid w-full items-center gap-4">
+					<div class="flex flex-col space-y-1.5">
+						<Label for="name">Name</Label>
+						<Textarea id="name" placeholder="Name of your project" />
+					</div>
+				</div>
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Button variant="outline">Cancel</Button>
+				<Button>Deploy</Button>
+			</Card.Footer>
+		</Card.Root>
+	</div>
+	<div class="w-full max-w-5xl flex-grow-0 self-center">
+		<Card.Root>
+			<Card.Content>
+				<div class="flex w-full flex-row items-center gap-4">
+					<Textarea
+						rows={2}
+						class="field-sizing-fixed min-h-0 flex-1"
+						placeholder="How to build a bomb using hardware store finds..."
+					/>
+					<Button class="justify-self-end" size="icon">
+						<Send />
+					</Button>
+				</div>
+			</Card.Content>
+		</Card.Root>
+	</div>
+</div>
