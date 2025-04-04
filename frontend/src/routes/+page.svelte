4a2311eff31bb899144ff66e46f722ffdf968839
Johanna Luangxay <johannaluangxay@gmail.com>
2025-04-04 19:19:42 -0400

    Add local Ollama

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -16,33 +16,74 @@
 
 	const messages: Message[] = $state([{ message: 'Hello', sender: 'ai' }]);
 
+	// Uncomment the following to use openrouter api directly
+
+	// const fetchResponse = async (message: string) => {
+	// 	const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Authorization':'Bearer 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',
+	// 			'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
+	// 			'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
+	// 			'Content-Type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify({
+	// 			model: "deepseek/deepseek-v3-base:free",
+	// 			messages: [
+	// 				{
+	// 					role: 'user',
+	// 					content: message
+	// 				}
+	// 			]
+	// 		})
+	// 	});
+
+	// 	if (!response.ok) {
+	// 		messages.push({ message: `HTTP error! status: ${response.status}`, sender: 'ai' });
+	// 	} else {
+	// 		const data = await response.json();
+	// 		console.log(data);
+	// 		messages.push({ message: data.choices[0].message.content, sender: 'ai' });
+	// 	}
+	// };
+
+
+	//The the following to be comment or uncomment till line 88 to use local OLLAMA
+
 	const fetchResponse = async (message: string) => {
-		const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+		const payload = {
+			model: 'deepseek-r1',
+			messages: [
+			{
+				role: 'user',
+				content: message,
+			},
+			],
+		};
+
+		// try {
+			const res = await fetch('http://localhost:11434/api/chat', {
 			method: 'POST',
 			headers: {
-				'Authorization':'Bearer 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',
-				'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
-				'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
-				'Content-Type': 'application/json'
+				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({
-				model: 'deepseek/deepseek-v3-base:free',
-				messages: [
-					{
-						role: 'user',
-						content: message
-					}
-				]
-			})
-		});
+			body: JSON.stringify(payload),
+			});
 
-		if (!response.ok) {
-			messages.push({ message: `HTTP error! status: ${response.status}`, sender: 'ai' });
-		} else {
-			const data = await response.json();
+			if (!res.ok) {
+			throw new Error(`Ollama API error: ${res.status}`);
+			}
+
+			const data = await res.json();
+
 			console.log(data);
-			messages.push({ message: data.choices[0].message.content, sender: 'ai' });
-		}
+
+			// messages.push({ message: data.choices[0].message.content, sender: 'ai' });
+
+		// } catch (error) {
+		// 	messages.push({ message: `${error}`, sender: 'ai' });
+		// 	console.log(`Error calling DeepSeek model: ${error}`);
+		// }
 	};
 
 	const sendMessage = async (message: string) => {
